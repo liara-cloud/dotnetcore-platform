700fb360ddc5381de35d6e1c0052e3a444e9d032
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-01-13 13:09:32 +0330

    Fix tagging

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 set -ex;
 
-docker build -t liararepo/dotnetcore-platform:builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=3.1 .
-docker build -t liararepo/dotnetcore-platform:runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=3.1 .
+docker build -t liararepo/dotnetcore-platform:3.1-builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=3.1 .
+docker build -t liararepo/dotnetcore-platform:3.1-runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=3.1 .
 
-docker build -t liararepo/dotnetcore-platform:builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=3.0 .
-docker build -t liararepo/dotnetcore-platform:runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=3.0 .
+docker build -t liararepo/dotnetcore-platform:3.0-builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=3.0 .
+docker build -t liararepo/dotnetcore-platform:3.0-runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=3.0 .
 
-docker build -t liararepo/dotnetcore-platform:builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=2.2 .
-docker build -t liararepo/dotnetcore-platform:runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=2.2 .
+docker build -t liararepo/dotnetcore-platform:2.2-builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=2.2 .
+docker build -t liararepo/dotnetcore-platform:2.2-runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=2.2 .
 
-docker build -t liararepo/dotnetcore-platform:builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=2.1 .
-docker build -t liararepo/dotnetcore-platform:runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=2.1 .
+docker build -t liararepo/dotnetcore-platform:2.1-builder -f Dockerfile.builder --build-arg DOTNETCORE_VERSION=2.1 .
+docker build -t liararepo/dotnetcore-platform:2.1-runtime -f Dockerfile.runtime --build-arg DOTNETCORE_VERSION=2.1 .
