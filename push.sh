d31a3d2e27141d671e63a5101507e4644005ed72
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-01-13 13:17:41 +0330

    Add push.sh

---
+++ push.sh
@@ -0,0 +1,13 @@
+set -ex;
+
+docker push liararepo/dotnetcore-platform:3.1-builder
+docker push liararepo/dotnetcore-platform:3.1-runtime
+
+docker push liararepo/dotnetcore-platform:3.0-builder
+docker push liararepo/dotnetcore-platform:3.0-runtime
+
+docker push liararepo/dotnetcore-platform:2.2-builder
+docker push liararepo/dotnetcore-platform:2.2-runtime
+
+docker push liararepo/dotnetcore-platform:2.1-builder
+docker push liararepo/dotnetcore-platform:2.1-runtime
