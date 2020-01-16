c345f5e94139e40c2f4f0a5fc5cf921e9cc6688a
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-01-16 14:11:50 +0330

    Minor fixes

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 BUILD_DIR=/src
 BUILD_CONFIGURATION=${BUILD_CONFIGURATION:-Release}
@@ -12,13 +12,16 @@ fi
 if [ -z ${__DOTNETCORE_PROJECT_FILE:-} ]; then
 	__DOTNETCORE_PROJECT_FILE=$(x=$(dirname $(find ${BUILD_DIR} -maxdepth 5 -iname Startup.cs -o -iname Program.cs | head -1)); while [[ "$x" =~ $BUILD_DIR ]] ; do find "$x" -maxdepth 1 -name *.csproj; x=`dirname "$x"`; done)
 fi
-if [ -z ${__DOTNETCORE_PROJECT_NAME:-} ]; then
-	__DOTNETCORE_PROJECT_NAME=$(basename ${__DOTNETCORE_PROJECT_FILE%.*})
-fi
-if [ -z ${__DOTNETCORE_PROJECT_NAME:-} ]; then
+
+if [ -z ${__DOTNETCORE_PROJECT_FILE:-} ]; then
 	echo "Could not find a .csproj file.";
 	exit 2;
 fi
+
+if [ -z ${__DOTNETCORE_PROJECT_NAME:-} ]; then
+	__DOTNETCORE_PROJECT_NAME=$(basename ${__DOTNETCORE_PROJECT_FILE%.*})
+fi
+
 if [ -n "$(cat $PROJECT_FILE | grep 'netcoreapp2.0')" ]; then
 	echo "======== WARNING ========"
 	echo "We don't support netcoreapp2.0. You may need to upgrade your framework version."
