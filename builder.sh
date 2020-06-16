d85e42d1a7863010e0f5e5d3fc6085f45ab460ff
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2020-06-16 14:16:43 +0430

    Rename build args

--- builder.sh
+++ builder.sh
@@ -9,17 +9,17 @@ if [ -f ${BUILD_DIR}/dotnet-tools.json ] || [ -f ${BUILD_DIR}/.config/dotnet-too
 	dotnet tool restore
 fi
 
-if [ -z ${__DOTNETCORE_PROJECT_FILE:-} ]; then
-	__DOTNETCORE_PROJECT_FILE=$(x=$(dirname $(find ${BUILD_DIR} -maxdepth 5 -iname Startup.cs -o -iname Program.cs | head -1)); while [[ "$x" =~ $BUILD_DIR ]] ; do find "$x" -maxdepth 1 -name *.csproj; x=`dirname "$x"`; done)
+if [ -z ${__NETCORE_CSPROJECTFILE:-} ]; then
+	__NETCORE_CSPROJECTFILE=$(x=$(dirname $(find ${BUILD_DIR} -maxdepth 5 -iname Startup.cs -o -iname Program.cs | head -1)); while [[ "$x" =~ $BUILD_DIR ]] ; do find "$x" -maxdepth 1 -name *.csproj; x=`dirname "$x"`; done)
 fi
 
-if [ -z ${__DOTNETCORE_PROJECT_FILE:-} ]; then
+if [ -z ${__NETCORE_CSPROJECTFILE:-} ]; then
 	echo "Could not find a .csproj file.";
 	exit 2;
 fi
 
-if [ -z ${__DOTNETCORE_PROJECT_NAME:-} ]; then
-	__DOTNETCORE_PROJECT_NAME=$(basename ${__DOTNETCORE_PROJECT_FILE%.*})
+if [ -z ${__NETCORE_FINALDLLNAME:-} ]; then
+	__NETCORE_FINALDLLNAME=$(basename ${__NETCORE_CSPROJECTFILE%.*})
 fi
 
 if [ -n "$(cat $PROJECT_FILE | grep 'netcoreapp2.0')" ]; then
@@ -27,6 +27,6 @@ if [ -n "$(cat $PROJECT_FILE | grep 'netcoreapp2.0')" ]; then
 	echo "We don't support netcoreapp2.0. You may need to upgrade your framework version."
 fi
 
-dotnet publish $__DOTNETCORE_PROJECT_FILE --output /app --configuration ${BUILD_CONFIGURATION}
+dotnet publish $__NETCORE_CSPROJECTFILE --output /app --configuration ${BUILD_CONFIGURATION}
 
-echo "${__DOTNETCORE_PROJECT_NAME}.dll" > /usr/local/app-name
+echo "${__NETCORE_FINALDLLNAME}.dll" > /usr/local/app-name
